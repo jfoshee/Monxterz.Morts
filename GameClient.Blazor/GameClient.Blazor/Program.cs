0365477219e66bac125a0fc2b7bbbd114d1ce8af
Jacob Foshee <jacobf@gmail.com>
2022-09-17 11:50:21 -0500

    chore(publish): Configuration for API base URL

--- GameClient.Blazor/GameClient.Blazor/Program.cs
+++ GameClient.Blazor/GameClient.Blazor/Program.cs
@@ -7,8 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddGameStateClientServices(Constants.GameMasterId, "https://localhost:7264/")
+var baseUrl = builder.Configuration["ApiBaseUrl"] ?? builder.HostEnvironment.BaseAddress;
+builder.Services.AddGameStateClientServices(Constants.GameMasterId, baseUrl)
                 .AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
